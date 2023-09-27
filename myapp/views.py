decc87c923bcd65d13b9c4c1499c287bbc8298cb
cyanvestige <cyangestige@gmail.com>
2023-09-27 14:29:48 +0900

    Showing every reizouko whose price is under the input price.

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic.base import TemplateView
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-import datetime
+import datetime, requests
 from .forms import MyForm
 
 # Create your views here.
@@ -32,9 +32,40 @@ def input_view(request):
             params['price_reizouko'] = form.cleaned_data['price_reizouko']
             params['price_denshi']  = form.cleaned_data['price_denshi']
             params['price_sentakuki'] = form.cleaned_data['price_sentakuki']
+            data = api(params)
             # データを使用して何かを行います
             # return redirect('myapp:view_page')  # データが正常に処理された場合、リダイレクト
-            return render(request, 'myapp/views.html',params)
+            return render(request, 'myapp/views.html', {'data': data})
     else:
         form = MyForm()  # GETリクエストの場合、空のフォームを表示
     return render(request, 'myapp/home.html', {'form': form})
+
+def api(params):
+    ITEM_GENRE_ID = {
+        'reizouko': 565161,
+        'sentakuki': 204491,
+        'denshi': 204585
+    }
+
+    APPLICATION_ID = "1098599347371457724"
+
+    RANK_API = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
+    ITEM_API = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
+
+    # Define the query parameters to get rezouko information
+    reizouko_params = {
+        "genreId": ITEM_GENRE_ID['reizouko'],
+        "maxPrice": params['price_reizouko'],
+        "elements": "itemCode,itemName,itemUrl,itemPrice,mediumImageUrls",
+        "applicationId": APPLICATION_ID,
+        "sort": "-itemPrice" # Sort by price in descending order 
+    }
+
+    try:
+        reizouko_response = requests.get(ITEM_API, params=reizouko_params)
+        if reizouko_response.status_code == 200:
+            reizouko_data = reizouko_response.json()
+            return reizouko_data["Items"]
+                        
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": f"Request Error: {e}"}, status=500)
