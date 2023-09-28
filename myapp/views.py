615d0e4b00fed1499f7856e0da0a3ba2a54e93ad
cyanvestige <cyangestige@gmail.com>
2023-09-28 10:20:46 +0900

    fix the form POST failure

--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render, redirect
-
-from django.http import HttpResponse, JsonResponse
-from django.views.generic.base import TemplateView
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-import datetime, requests
+from django.http import JsonResponse
+import requests
 from .forms import MyForm
-
-# Create your views here.
-# class Home(TemplateView):
-#     template_name = "myapp/home.html"
 
 APPLICATION_ID = "1098599347371457724"
 
@@ -17,32 +9,25 @@ RANK_API = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
 
 
 def success_view(request):
-    # 入力値をコンテキストに追加
-    params = {}
-    params['genre'] = request.GET.get("genre")
-    params['price_reizouko'] = request.GET.get("price_reizouko")
-    params['price_denshi'] = request.GET.get("price_denshi")
-    params['price_sentakuki'] = request.GET.get("price_sentakuki")
-    # densirenjiPrice,reizoukoPrice,sentakutukiPrice = request.GET.get('densirenjiPrice', 'reizoukoPrice',"sentakukiPrice")
-    return render(request, 'myapp/views.html', params)
+    if request.method == 'POST':
+        params = {}
+        params['genre'] = request.POST.get("genre")
+        params['price_reizouko'] = request.POST.get("price_reizouko")
+        params['price_denshi'] = request.POST.get("price_denshi")
+        params['price_sentakuki'] = request.POST.get("price_sentakuki")
+        data = api(params)
 
+        return render(request, 'myapp/views.html', {'data': data })
 
 def input_view(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            # フォームがバリデーションを通過した場合、データを処理
-            params = {}
-            params['genre'] = form.cleaned_data['genre']
-            params['price_reizouko'] = form.cleaned_data['price_reizouko']
-            params['price_denshi']  = form.cleaned_data['price_denshi']
-            params['price_sentakuki'] = form.cleaned_data['price_sentakuki']
-            data = api(params)
-            # データを使用して何かを行います
-            # return redirect('myapp:view_page')  # データが正常に処理された場合、リダイレクト
-            return render(request, 'myapp/views.html', {'data': data })
+            # データが正常に処理された場合、リダイレクト
+            return redirect('myapp:view_page')  
     else:
         form = MyForm()  # GETリクエストの場合、空のフォームを表示
+
     return render(request, 'myapp/home.html', {'form': form})
 
 def api(params):
@@ -63,6 +48,8 @@ def api(params):
 
 def get_products_info(price, genre_id, sort):
     
+    price = int(price)
+
     products = []
 
     query_params = {
@@ -91,6 +78,5 @@ def get_products_info(price, genre_id, sort):
                 products.append(product)
             return products
              
-               
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": f"Request Error: {e}"}, status=500)
