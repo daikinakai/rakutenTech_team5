fd4811558d5cbf94e7619d3a98599f2135e12078
daikinakai <daikisoccer1106@gmail.com>
2023-09-28 11:57:21 +0900

    main merge

--- myapp/forms.py
+++ myapp/forms.py
@@ -10,3 +10,27 @@ class MyForm(forms.Form):
     price_reizouko = forms.IntegerField()
     price_denshi = forms.IntegerField()
     price_sentakuki = forms.IntegerField()
+    price_suihanki = forms.IntegerField()
+    price_television = forms.IntegerField()
+    price_soujiki = forms.IntegerField()
+    price_hair_dryer = forms.IntegerField()
+
+class CheckForm(forms.Form):
+    data = [('home appliances', 'home appliances')]
+    genre = forms.ChoiceField(label='genre', choices=data)
+
+    home_appliances_choices = [
+    ("refrigerator_price", "refrigerator"),
+    ("microwave_oven_price", "microwave oven"),
+    ("washing_machine_price", "washing machine"),
+    ]
+
+    Need_items = forms.MultipleChoiceField(
+        required=True,
+        widget=forms.CheckboxSelectMultiple,
+        choices=home_appliances_choices,
+    )
+            
+    
+class PriceForm(forms.Form):
+    pass
--- myapp/views.py
+++ myapp/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import requests
+from .forms import CheckForm, PriceForm,MyForm
+from django import forms
 
-from django.http import HttpResponse, JsonResponse
-from django.views.generic.base import TemplateView
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-import datetime, requests
-from .forms import MyForm
-
-# Create your views here.
-# class Home(TemplateView):
-#     template_name = "myapp/home.html"
 
 APPLICATION_ID = "1098599347371457724"
 
@@ -17,45 +11,102 @@ RANK_API = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601"
 
 
 def success_view(request):
-    # 入力値をコンテキストに追加
-    params = {}
-    params['genre'] = request.GET.get("genre")
-    params['price_reizouko'] = request.GET.get("price_reizouko")
-    params['price_denshi'] = request.GET.get("price_denshi")
-    params['price_sentakuki'] = request.GET.get("price_sentakuki")
-    # densirenjiPrice,reizoukoPrice,sentakutukiPrice = request.GET.get('densirenjiPrice', 'reizoukoPrice',"sentakukiPrice")
-    return render(request, 'myapp/views.html', params)
+    if request.method == 'POST':
+        params = {}
+        params['genre'] = request.POST.get("genre")
+        params['price_reizouko'] = request.POST.get("price_reizouko")
+        params['price_denshi'] = request.POST.get("price_denshi")
+        params['price_sentakuki'] = request.POST.get("price_sentakuki")
+        params['price_suihanki'] = request.POST.get("price_suihanki")
+        params['price_television'] = request.POST.get("price_television")
+        params['price_soujiki'] = request.POST.get("price_soujiki")
+        params['price_hair_dryer'] = request.POST.get("price_hair_dryer")
+
+        data = api(params)
+
+        return render(request, 'myapp/views.html', {'data': data })
 
 
+
+# def input_view(request):
+#     params = {
+#         'headtitle' : 'team5',
+#         'title' : 'Select genre & budget!',
+#         'form' : CheckForm(),
+#         'btn' : 'select',
+#     }
+
+#     if request.method == 'POST':
+#         keys = request.POST.keys()
+#         if 'genre' in keys and 'Need_items' in keys:
+#             form = CheckForm(request.POST)
+#             if form.is_valid():
+#                 params = {
+#                     'headtitle' : 'team5',
+#                     'title' : 'Select genere & budget!',
+#                     'btn' : 'get recommend',
+#                 } 
+#                 temp = form.cleaned_data.get('Need_items')
+
+#                 dyn_form = PriceForm()
+#                 for k in temp:
+#                     dyn_form.fields[k.replace('_',' ')] = forms.IntegerField(\
+#                                 min_value=0)
+#                 dyn_form.fields['budget_over'] = forms.BooleanField(required=False)
+#                 params['form'] = dyn_form
+#                 return render(request, 'myapp/home.html', params)
+#             else:
+
+#                 return render(request, 'myapp/home.html', params)
+#         # 金額入力
+#         elif 'refrigerator price' in keys\
+#             or 'microwave oven price' in keys\
+#                 or 'washing machine price' in keys:
+
+#             for k in keys:
+#                 if k != 'csrfmiddlewaretoken':
+#                     params[k] = request.POST[k]
+
+            
+#             params['title'] = 'view_list'
+#             params['headtitle'] = 'team5'
+#             del params['form'], params['btn']
+            
+#             return redirect('myapp:view_page')  
+            
+            
+#     else:
+#         return render(request, 'myapp/home.html', params)
 def input_view(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            # フォームがバリデーションを通過した場合、データを処理
-            params = {}
-            # params['genre'] = form.cleaned_data['genre']
-            params['price_reizouko'] = form.cleaned_data['price_reizouko']
-            params['price_denshi']  = form.cleaned_data['price_denshi']
-            params['price_sentakuki'] = form.cleaned_data['price_sentakuki']
-            data = api(params)
-            # データを使用して何かを行います
-            # return redirect('myapp:view_page')  # データが正常に処理された場合、リダイレクト
-            return render(request, 'myapp/views.html', {'data': data })
+            # データが正常に処理された場合、リダイレクト
+            return redirect('myapp:view_page')  
     else:
         form = MyForm()  # GETリクエストの場合、空のフォームを表示
+
     return render(request, 'myapp/home.html', {'form': form})
 
 def api(params):
     ITEM_GENRE_ID = {
         'reizouko': 565161,
         'sentakuki': 204491,
-        'denshi': 204585
+        'denshi': 204585,
+        'suihanki': 204586,
+        'television': 563843,
+        'soujiki': 204492,
+        'hair_dryer': 502792,
     }
     
     products_info = {
         'reizouko': get_products_info(params['price_reizouko'], ITEM_GENRE_ID['reizouko'], 'limit'),
         'sentakuki':get_products_info(params['price_sentakuki'], ITEM_GENRE_ID['sentakuki'], 'limit'),
-        'denshi': get_products_info(params['price_denshi'], ITEM_GENRE_ID['denshi'], 'limit')
+        'denshi': get_products_info(params['price_denshi'], ITEM_GENRE_ID['denshi'], 'limit'),
+        'suihanki': get_products_info(params['price_suihanki'], ITEM_GENRE_ID['suihanki'], 'limit'),
+        'television': get_products_info(params['price_television'], ITEM_GENRE_ID['television'], 'limit'),
+        'soujiki': get_products_info(params['price_soujiki'], ITEM_GENRE_ID['soujiki'], 'limit'),
+        'hair_dryer': get_products_info(params['price_hair_dryer'], ITEM_GENRE_ID['hair_dryer'], 'limit'),
     }
 
     return products_info
@@ -63,6 +114,8 @@ def api(params):
 
 def get_products_info(price, genre_id, sort):
     
+    price = int(price)
+
     products = []
 
     query_params = {
@@ -91,6 +144,5 @@ def get_products_info(price, genre_id, sort):
                 products.append(product)
             return products
              
-               
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": f"Request Error: {e}"}, status=500)
